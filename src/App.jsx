be85e76eadb5d31eb3370c4ae75a1526e52995c6
KalyanSreekar <kalyansreekarj@gmail.com>
2024-01-08 21:18:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-600 flex items-center justify-center">
+      <p>Stealth AI project setup</p>
+    </div>
+  );
+}
+
+export default App;
